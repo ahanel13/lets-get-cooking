646df796ce780ba47ba1abc9dd00b236ada9304b
Anthony <ahanel13@gmail.com>
2021-02-20 12:10:14 -0600

    finished adding a functional clearing button for the comment box

--- app/javascript/packs/recipes.js
+++ app/javascript/packs/recipes.js
@@ -1,11 +1,3 @@
-function hello(name) {
-    console.log("Hello " + name + "!");
-  }
-
-function greet_user(last_name, first_name) {
-    hello(last_name + " " + first_name);
-  }
-  /* Or the ES6 version for this: */
-  $(() =>
-    $('button#greet-user-button').on('click', () => greet_user('Dire', 'Strait'))
-  );
+window.clearCommentArea = function(){
+  document.getElementById("comment_textarea").value="";
+}
